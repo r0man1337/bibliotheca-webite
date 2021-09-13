93556c7c37737176f1b1b81ce89220efebfee9c6
pondering democritus <ponderingdemocritus@protonmail.com>
2021-09-13 16:24:40 +1000

    add buttons

--- components/web3/AssetPill.vue
+++ components/web3/AssetPill.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="rounded border border-gray-400 p-2 my-4">#{{ asset.id }}</div>
+  <button
+    class="
+      rounded
+      border border-gray-400
+      px-4
+      py-3
+      my-4
+      flex
+      justify-between
+      w-full
+      hover:bg-gray-600
+    "
+  >
+    <span class="self-center">#{{ asset.id }}</span>
+    <LoadingRings v-if="loading" />
+  </button>
 </template>
 <script>
 import { defineComponent } from '@vue/composition-api'
-
+import LoadingRings from '~/assets/img/loadingRings.svg?inline'
 export default defineComponent({
+  components: {
+    LoadingRings,
+  },
   props: {
     asset: {
       type: Object,
       required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
   },
   setup() {},
