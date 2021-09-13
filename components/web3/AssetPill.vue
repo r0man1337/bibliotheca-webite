fb2be7243c9f40255f6b018d05294e4051679bc3
pondering democritus <ponderingdemocritus@protonmail.com>
2021-09-13 16:15:18 +1000

    UI interations and network switching

---
+++ components/web3/AssetPill.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="rounded border border-gray-400 p-2 my-4">#{{ asset.id }}</div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    asset: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {},
+})
+</script>
--- pages/bridge/arbitrum.vue
+++ pages/bridge/arbitrum.vue
@@ -4,42 +4,66 @@
       <h1>Arbitrum Warp Bridge</h1>
       <h5 class="text-xl">
         You are currently connected to
-        <span class="tex">{{ activeNetwork.name }}</span>
+        <span class="tex">{{ networkName }}</span>
       </h5>
     </div>
     <div class="flex mt-8 flex-wrap">
       <div class="sm:w-5/12 flex justify-around">
         <div>
-          <h4 class="mb-4">Layer 1</h4>
-          <div class="bg-black rounded-2xl p-4 w-80">
+          <h4 class="mb-4 flex">
+            Ethereum Layer 1 <Lock v-if="networkId === 4" class="ml-4" />
+          </h4>
+          <div
+            :class="{ 'opacity-75 bg-gray-200 text-black': networkId === 4 }"
+            class="bg-black rounded-2xl p-4 w-80"
+          >
             <h4 class="mb-4">Realms</h4>
-            <div class="rounded border border-gray-400 p-2 my-4">#1242</div>
-            <div class="rounded border border-gray-800 bg-gray-800 p-2 my-2">
-              #122
-            </div>
+            <AssetPill
+              v-for="(asset, index) in assetsOnL1"
+              :key="index"
+              :asset="asset"
+            />
           </div>
         </div>
       </div>
       <div class="sm:w-2/12 flex flex-col justify-around my-4">
         <div class="flex">
-          <ArrowLeft class="w-8 h-8 self-center mx-2 rotate-180 transform" />
+          <ArrowLeft
+            :class="{
+              'rotate-180': networkId === 1,
+            }"
+            class="w-8 h-8 self-center mx-2 transform"
+          />
           <div class="text-center w-full">
             <button class="rounded px-4 py-2 bg-gray-700 w-full">
-              Warp To Arbitrum
+              {{
+                networkId === 1 ? 'Warp To Arbitrum L2' : 'Warp To Ethereum L1'
+              }}
             </button>
           </div>
-          <ArrowRight class="w-8 h-8 self-center mx-2" />
+          <ArrowRight
+            :class="{
+              'rotate-180': networkId === 4,
+            }"
+            class="w-8 h-8 self-center mx-2 transform"
+          />
         </div>
       </div>
       <div class="sm:w-5/12 flex justify-around">
         <div>
-          <h4 class="mb-4">Layer 2</h4>
-          <div class="bg-black rounded-2xl p-4 w-80">
+          <h4 class="mb-4 flex">
+            Arbitrum Layer 2 <Lock v-if="networkId === 1" class="ml-4" />
+          </h4>
+          <div
+            :class="{ 'opacity-75 bg-gray-200 text-black': networkId === 1 }"
+            class="bg-black rounded-2xl p-4 w-80"
+          >
             <h4 class="mb-4">Realms</h4>
-            <div class="rounded border border-gray-400 p-2 my-4">#1242</div>
-            <div class="rounded border border-gray-800 bg-gray-800 p-2 my-2">
-              #122
-            </div>
+            <AssetPill
+              v-for="(asset, index) in assetsOnL2"
+              :key="index"
+              :asset="asset"
+            />
           </div>
         </div>
       </div>
@@ -47,20 +71,54 @@
   </div>
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, computed } from '@nuxtjs/composition-api'
 import ArrowRight from '~/assets/img/arrow-right.svg?inline'
 import ArrowLeft from '~/assets/img/arrow-left.svg?inline'
+import Lock from '~/assets/img/lock.svg?inline'
 import { useNetwork } from '~/composables/web3/useNetwork'
+
 export default defineComponent({
   components: {
     ArrowRight,
     ArrowLeft,
+    Lock,
   },
 
   setup() {
     const { activeNetwork } = useNetwork()
+
+    const networkName = computed(() => {
+      return activeNetwork.value.name
+    })
+
+    const networkId = computed(() => {
+      return activeNetwork.value.chainId
+    })
+
+    const assetsOnL1 = [
+      {
+        id: 123,
+      },
+      {
+        id: 2312,
+      },
+    ]
+
+    const assetsOnL2 = [
+      {
+        id: 232,
+      },
+      {
+        id: 2222,
+      },
+    ]
+
     return {
       activeNetwork,
+      networkId,
+      networkName,
+      assetsOnL2,
+      assetsOnL1,
     }
   },
 })
