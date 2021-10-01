55fac55ed792ac0ac8dee44290c1dfd32b483d5c
pondering democritus <ponderingdemocritus@protonmail.com>
2021-10-02 05:51:01 +1000

    remove mLoot

--- pages/adventurer/index.vue
+++ pages/adventurer/index.vue
@@ -83,7 +83,7 @@ export default defineComponent({
             bagsHeld
             treasuresHeld
             manasHeld
-            mLootHeld
+            # mLootHeld
           }
         }
       `)
